4a0441afc9aa1b371f71a25e58a6ceda07e2d46b
Andrew Watkins <watts@influxdb.com>
2016-03-08 14:44:40 -0800

    Add password validation

---
+++ app/components/PasswordValidator/PasswordValidator.js
@@ -0,0 +1,37 @@
+import React, {Component} from 'react';
+import classNames from 'classnames';
+
+import './password-validator.css';
+
+export default class PasswordValidator extends Component {
+  constructor(props) {
+    super(props)
+  }
+
+  render() {
+    const {password} = this.props
+    const isLongEnough = password.length > 8
+    const hasUpperAndLowercase = password.match(/[A-Z]/) && password.match(/[a-z]/)
+    const hasNumber = password.match(/[0-9]/)
+    const allMatch = isLongEnough && hasUpperAndLowercase && hasNumber
+
+    return (
+      <div className="password-validator">
+        <div className={classNames({"error": !isLongEnough}, {"success": isLongEnough})}>
+          <div className="icon icon-green-check" />
+          <div className="message">8 or more characters </div>
+        </div>
+
+        <div className={classNames({"error": !hasUpperAndLowercase}, {"success": hasUpperAndLowercase})}>
+          <div className="icon icon-green-check" />
+          <div className="message">Upper and lowercase</div>
+        </div>
+
+        <div className={classNames({"error": !hasNumber}, {"success": hasNumber})}>
+          <div className="icon icon-green-check" />
+          <div className="message">At least 1 number</div>
+        </div>
+      </div>
+    );
+  }
+}
--- app/components/Register/Register.js
+++ app/components/Register/Register.js
@@ -1,15 +1,24 @@
 import React, {Component} from 'react';
+import PasswordValidator from 'components/PasswordValidator/PasswordValidator';
+import ReactDOM from 'react-dom';
 
 import './register.css';
 
 export default class Register extends Component {
-   constructor(props) {
-    super(props);
+  constructor() {
+    super()
+
+    this.state = {
+      isPasswordFocused: false,
+      password: ""
+    }
   }
 
   render() {
+    const {password} = this.state;
+
     return (
-      <form className="form-register" method="post" action="#">
+      <form className="form-register" method="post" action="#" autoComplete="on">
         <div className="form-register-with-email">
           <div className="form-white-background">
 
@@ -18,28 +27,45 @@ export default class Register extends Component {
             </div>
 
             <div className="form-row">
-              <label for="name">
+              <label htmlFor="first-name">
                 <span>Name</span>
-                <input ref="name" type="text" name="name" id="name" autoFocus required />
+                <input placeholder="first name" ref="name" type="text" name="first-name" id="name" autoComplete="given-name" autoFocus required />
               </label>
             </div>
 
             <div className="form-row">
-              <label for="email">
+              <label htmlFor="email">
                 <span>Email</span>
-                <input type="email" name="email" id="email" required />
+                <input placeholder="name@example.com" type="email" name="email" id="email" autoComplete="email" required />
               </label>
             </div>
 
             <div className="form-row">
-              <label for="password">
+              <label htmlFor="company">
+                <span>Company</span>
+                <input placeholder="(optional) where do you work?" type="text" name="company" id="company" />
+              </label>
+            </div>
+
+            <div className="form-row">
+              <label htmlFor="password">
                 <span>Password</span>
-                <input type="password" name="password" id="password" onFoucs={this.renderPasswordValidation} required />
+                <input
+                  ref="password"
+                  type="password"
+                  name="password"
+                  id="password"
+                  onBlur={this.removePasswordValidation}
+                  onFocus={this.renderPasswordValidation}
+                  onChange={this.getPassword}
+                  required
+                />
+                <PasswordValidator password={password} />
               </label>
             </div>
 
             <div className="form-row">
-              <button type="submit">Register</button>
+              <button type="submit" onSubmit={this.handleRegister}>Register</button>
             </div>
           </div>
           <a href="#" className="form-log-in-with-existing">Already have an account? Login here &rarr;</a>
@@ -47,4 +73,18 @@ export default class Register extends Component {
       </form>
     );
   }
+
+  renderPasswordValidation = () => {
+    this.setState({isPasswordFocused: true})
+  };
+
+  removePasswordValidation = () => {
+    this.setState({isPasswordFocused: false})
+  };
+
+  getPassword = () => {
+    const password = ReactDOM.findDOMNode(this.refs.password)
+    this.setState({password: password.value || ""})
+  };
+
 }
